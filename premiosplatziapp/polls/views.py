40702fa61f31a88606bcb292524363c7127a7ffd
Rafa Niquefa <127156778+m8niquefa@users.noreply.github.com>
2023-03-31 21:36:50 -0500

    Just finished lesson 24

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-
+from .models import Question
 
 def index(request):
-    return HttpResponse(f"Hello, world. You're at the polls index. {str(request)}")
+    latest_question_list = Question.objects.all()
+    return render(request, "polls/index.html", {"latest_question_list": latest_question_list})
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
+
+def results(request, question_id):
+    return HttpResponse(f"You're looking at the results of question {question_id}.\n request taken: {str(request)}")
+
+
+def vote(request, question_id):
+    return HttpResponse(f"You're voting on question {question_id}.\n request taken: {str(request)}")
