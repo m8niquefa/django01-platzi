f7d20cfa3c585a0de9a2e21d6920e71a9f30bbdc
Rafa Niquefa <127156778+m8niquefa@users.noreply.github.com>
2023-04-02 00:24:34 -0500

    Finish lesson 15 course 2

--- premiosplatziapp/polls/admin.py
+++ premiosplatziapp/polls/admin.py
@@ -1,4 +1,25 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, Choice
 
-admin.site.register(Question)
+# To each Choice, provide an extra form so that data can be entered inline with three answers per question:
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+# Register your models here. To make a model visible on the admin site, register the model with admin.site.register():
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ["pub_date", "question_text"]
+    inlines = [ChoiceInline]
+
+    #To see extra information in the django admin
+    list_display = ("question_text", "pub_date", "was_published_recently")
+    
+    #To add filters to the admin page
+    list_filter = ["pub_date"]
+
+    #To add a search bar to the admin page
+    search_fields = ["question_text"]
+
+
+
+admin.site.register(Question, QuestionAdmin)
